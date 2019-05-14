fea9c98645b4123098fb1ec6b1c93a38552464e2
Rahul Monde <rahul.monde@globant.com>
2019-05-14 15:08:42 +0530

    Removed credentials

--- instance.tf
+++ instance.tf
@@ -1,6 +1,4 @@
 provider "aws" {
-    access_key = "AKIAROV6LAQFZP7PUAEY"
-    secret_key = "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"
     region = "us-east-1"
 }
 resource "aws_instance" "myEC2Instace" {
